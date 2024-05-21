eddcf86ece2406b466074975d94d3f95ea2c8073
Pedro Viveiros <pedrohr.viveiros@gmail.com>
2024-05-21 11:33:21 -0300

    Atualiza script de setup

--- local_setup.bat
+++ local_setup.bat
@@ -8,6 +8,6 @@ echo Caso contrario, aperte Ctrll+C para sair
 pause
 cd backend
 py -m pip install pipenv
-pipenv shell --python 3.10
-pipenv run pre-commit install 
-pipenv install --dev
+pip install pre-commit
+pipenv install --dev --python 3.10
+pipenv run pre-commit install
