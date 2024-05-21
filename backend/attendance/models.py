8da3daa88d516e1770505584ed444f06d3daf09d
Pedro Viveiros <pedrohr.viveiros@gmail.com>
2024-05-21 13:05:40 -0300

    Adiciona import do _ e corrige related_name

--- backend/attendance/models.py
+++ backend/attendance/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
 class Subject(models.Model):
@@ -8,14 +10,14 @@ class Subject(models.Model):
 class Class(models.Model):
     name = models.CharField(max_length=30, unique=True)
     classroom = models.CharField(max_length=10, blank=True, null=True)
-    course = models.CharField(max_length=10) # TODO: criar modelo com ChoiceField
+    course = models.CharField(max_length=10)  # TODO: criar modelo com ChoiceField
     subjects = models.ManyToManyField(Subject, related_name="classes")
 
 
 class Student(models.Model):
     full_name = models.CharField(max_length=100, null=False, blank=False)
-    course_class = models.OneToOneField(Class, on_delete=models.CASCADE, related_name="class")
-    
+    course_class = models.OneToOneField(Class, on_delete=models.CASCADE, related_name="students")
+
 
 class Lesson(models.Model):
     course_class = models.ForeignKey(Class, on_delete=models.CASCADE, related_name="lessons")
@@ -30,12 +32,11 @@ class LessonDatetime(models.Model):
 
 class Attendance(models.Model):
     class AttendanceChoices(models.TextChoices):
-        PRESENT = 'P', _('Present')
-        ABSENT = 'A', _('Absent')
-        JUSTIFIED = 'J', _('Justified')
+        PRESENT = "P", _("Present")
+        ABSENT = "A", _("Absent")
+        JUSTIFIED = "J", _("Justified")
 
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='attendances')
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='attendances')
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="attendances")
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name="attendances")
     datetime = models.DateTimeField(auto_now_add=True, blank=True)
-    status = models.CharField(max_length=1, choices=AttendanceChoices, default=AttendanceChoices.ABSENT)    
-    
+    status = models.CharField(max_length=1, choices=AttendanceChoices, default=AttendanceChoices.ABSENT)
