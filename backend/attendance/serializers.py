a47ab507ba73d4644f271ac45643d27e192f3dd5
Pedro Viveiros <pedrohr.viveiros@gmail.com>
2024-05-21 15:06:49 -0300

    Altera serializers

--- backend/attendance/serializers.py
+++ backend/attendance/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
 
-from .models import (
-    Lesson,
-)
+from .models import Class, Lesson, Subject
+
+
+class ClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Class
+        fields = "__all__"
+
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Class
+        fields = "__all__"
+
 
 class LessonSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Lesson
-        fields = '__all__'
+        fields = "__all__"
