3efa832e5dc7eabde0563f4adcbbdc5fc4fc9cd9
VictorASantana <victor.asantana012@gmail.com>
2024-05-24 12:52:31 -0300

    fix: implementing requested changes

--- backend/attendance/views.py
+++ backend/attendance/views.py
@@ -11,43 +11,5 @@ class LessonView(ModelViewSet):
     serializer_class = LessonSerializer
 
 class StudentView(ModelViewSet):
-    def create(self, request): 
-        serializer = StudentSerializer(data=request.data)
-        if not serializer.is_valid(): 
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        student = Student.objects.create(**serializer.validated_data)
-        student_serialized = StudentSerializer(student)
-
-        return Response(student_serialized.data, status=status.HTTP_201_CREATED)
-    
-    def list_all(self, request):
-        student_list = Student.objects.all()
-
-        if not student_list: 
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = StudentSerializer(student_list, many=True)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def retrieve(self, request, pk):
-        student = Student.objects.filter(pk=pk).first()
-
-        if not student:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        serializer = StudentSerializer(student)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def delete(self, request, pk): 
-        queryset = Student.objects.filter(pk=pk).first()
-
-        if not queryset: 
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        
-        queryset.delete()
-        serializer = StudentSerializer(queryset)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-        
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
