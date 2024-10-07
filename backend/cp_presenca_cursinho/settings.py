682c71eecc58316f5cc606eb9aee4fdfcec1364c
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-07 06:47:32 -0300

    🔧 Updated timezone configuration

--- backend/cp_presenca_cursinho/settings.py
+++ backend/cp_presenca_cursinho/settings.py
@@ -121,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
@@ -139,4 +139,3 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(" ")
-
