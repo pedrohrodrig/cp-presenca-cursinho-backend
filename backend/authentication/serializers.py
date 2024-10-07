c6eabe87395af926dad0c068f6426706a600074a
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-10-06 22:29:12 -0300

    fix: remove password from serializer

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -2,6 +2,7 @@ from .models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
