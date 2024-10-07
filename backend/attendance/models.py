0b71138753f4d5d9407b6d22e0d87d186b395d6a
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-07 01:14:03 -0300

    🚀 Updated Lesson and LessonRecurrency models

--- backend/attendance/models.py
+++ backend/attendance/models.py
@@ -1,9 +1,11 @@
+from datetime import datetime
+
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from authentication.models import User
-
 
 # Create your models here.
 class Subject(models.Model):
@@ -87,16 +89,16 @@ class LessonRecurrency(models.Model):
 
 class LessonRecurrentDatetime(models.Model):
     lesson_recurrency = models.ForeignKey(LessonRecurrency, on_delete=models.CASCADE, related_name="regular_datetimes")
-    datetime = models.DateTimeField()
-
-    @property
-    def day_of_week(self):
-        # 0 = monday / 6 = sunday
-        return self.datetime.weekday()
+    start_datetime = models.DateTimeField(default=datetime.now())
+    end_datetime = models.DateTimeField(default=datetime.now())
+    day_of_week = models.IntegerField(default=0, validators=[MaxValueValidator(6), MinValueValidator(0)])
 
 
 class Lesson(models.Model):
     lesson_recurrency = models.ForeignKey(LessonRecurrency, on_delete=models.CASCADE, related_name="lessons")
+    lesson_recurrent_datetime = models.ForeignKey(
+        LessonRecurrentDatetime, on_delete=models.CASCADE, related_name="lessons", blank=True, null=True
+    )
     name = models.CharField(max_length=100)
     start_datetime = models.DateTimeField()
     end_datetime = models.DateTimeField()
