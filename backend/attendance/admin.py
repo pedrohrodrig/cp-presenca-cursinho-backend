a558e92461c41e7fcdd1144aa539ac287d262158
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-07 01:14:34 -0300

    ✨ Added LessonRecurrencyDatetime to admin

--- backend/attendance/admin.py
+++ backend/attendance/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import Attendance, StudentClass, Lesson, LessonRecurrency, Student, Subject
+from .models import Attendance, Lesson, LessonRecurrency, LessonRecurrentDatetime, Student, StudentClass, Subject
 
 # Register your models here.
 admin.site.register(Attendance)
 admin.site.register(StudentClass)
 admin.site.register(Lesson)
 admin.site.register(LessonRecurrency)
+admin.site.register(LessonRecurrentDatetime)
 admin.site.register(Subject)
 admin.site.register(Student)
