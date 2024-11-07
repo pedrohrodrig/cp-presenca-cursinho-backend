83204972d0fd7eb6b64c2fd289ea75fe4cc8a873
Lucas Garieri <lucaspvg1@gmail.com>
2024-11-07 14:22:07 -0300

    🚀 Add student filters

--- backend/attendance/filters.py
+++ backend/attendance/filters.py
@@ -1,6 +1,8 @@
 import django_filters
+from django.db.models import Value
+from django.db.models.functions import Concat
 
-from .models import Lesson
+from .models import Lesson, Student
 
 
 class LessonFilter(django_filters.FilterSet):
@@ -14,3 +16,17 @@ class LessonFilter(django_filters.FilterSet):
         fields = {
             "start_datetime": ["lte", "gte"],
         }
+
+
+class StudentFilter(django_filters.FilterSet):
+    student_class = django_filters.CharFilter(field_name="student_class", lookup_expr="name__exact")
+    name = django_filters.CharFilter(method="filter_full_name")
+
+    class Meta:
+        model = Student
+        fields = {}
+
+    def filter_full_name(self, queryset, name, value):
+        queryset = queryset.annotate(full_name=Concat("user__first_name", Value(" "), "user__last_name"))
+
+        return queryset.filter(full_name__icontains=value)
