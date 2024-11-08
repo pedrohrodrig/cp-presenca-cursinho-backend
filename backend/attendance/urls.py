c6a585e25fd0fc88acd701ab342bf251e3fabb54
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-11-08 15:05:08 -0300

    fix: mobile

--- backend/attendance/urls.py
+++ backend/attendance/urls.py
@@ -46,7 +46,7 @@ urlpatterns = format_suffix_patterns(
             StudentView.as_view({"get": "get_student"}),
         ),
         path("subject/", SubjectView.as_view({"get": "list", "post": "create_subject_and_recurrency"})),
-        path("subject_with_details/", SubjectView.as_view({"get": "list_subject_with_details"})),
+        path("subject_with_details/<int:student_class>", SubjectView.as_view({"get": "list_subject_with_details"})),
         path(
             "subject/<int:pk>/",
             SubjectView.as_view({"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}),
