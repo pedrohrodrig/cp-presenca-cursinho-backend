d8413452be7cde4e2067f75c61d86962a43bf49b
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-11-07 23:41:56 -0300

    fix: lets gooooo

--- backend/cp_presenca_cursinho/settings.py
+++ backend/cp_presenca_cursinho/settings.py
@@ -145,8 +145,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(" ")
-
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
