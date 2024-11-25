e88cef6763a15da798f11857e434bb3aceff94fb
Lucas Garieri <lucaspvg1@gmail.com>
2024-11-25 16:41:42 -0300

    ✨ Add metrics and tested one metric view

--- backend/attendance/urls.py
+++ backend/attendance/urls.py
@@ -10,6 +10,7 @@ from .views import (
     StudentClassView,
     StudentView,
     SubjectView,
+    students_total_attendance_percentage,
 )
 
 urlpatterns = format_suffix_patterns(
@@ -93,5 +94,6 @@ urlpatterns = format_suffix_patterns(
                 }
             ),
         ),
+        path("metrics/students-total-attendance/", students_total_attendance_percentage),
     ]
 )
